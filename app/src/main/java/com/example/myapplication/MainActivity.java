da4040c63df0beb42f40c25530976c3f5f222c33
黃妏婕 <shy4429888@gmail.com>
2019-03-19 13:50:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    int size = 30;//sp
+
+    public void bigger(View v){
+        TextView hello;
+        hello = findViewById(R.id.textView);
+        hello.setTextSize(++size);
+    }
+}
